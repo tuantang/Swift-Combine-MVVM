073ce0437f7d117fd0652d572cc3442dc818b752
Tuan Tang <tbtuan@enjoyworks.co.kr>
2022-12-21 11:21:18 +0700

    initial code

---
+++ Swift-Combine-MVVM-iOS/Swift-Combine-MVVM-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-Combine-MVVM-iOS
+//
+//  Created by Tuan Tang on 21/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift-Combine-MVVM-iPadOS/Swift-Combine-MVVM-iPadOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-Combine-MVVM-iPadOS
+//
+//  Created by Tuan Tang on 21/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
